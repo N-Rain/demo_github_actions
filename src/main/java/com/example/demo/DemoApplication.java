378df1e6a4673d5bf942d17d0453680c18cacbbf
Ngoc Pham <bngoc1792@gmail.com>
2024-05-20 22:04:59 +0700

    ...

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+	@GetMapping
+	public String welcome(){
+		return "Hello Build & Push Docker Image use Spring boot application using Github Actions ";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
